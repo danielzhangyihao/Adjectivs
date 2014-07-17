7a25f8e4696f898b6a1a464722dd65401048c5bc
daniel <zhangyihao1009@gmail.com>
2014-07-17 12:08:25 -0700

    working with amazon but did not finish

---
+++ spec/repositories/amazon_product_repository_spec.rb
@@ -0,0 +1,9 @@
+#require 'spec_helper'
+
+#describe AmazonProductRepository do
+	#it 'can fetch product from Amazon' do
+		#amazon_product_repository = AmazonProductRepository.new
+		#product = amazon_product_repository.fetch('B00008OE6I')
+		#expect(product.name).to eq('Canon PowerShot S400 4MP Digital Camera w/ 3x Optical Zoom')
+	#end
+#end
