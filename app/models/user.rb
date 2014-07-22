e29a3f52626ae341ba314d44999976283ef9dd38
daniel <zhangyihao1009@gmail.com>
2014-07-22 12:22:28 -0700

    asset changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :s3_credentials => Proc.new{|a| a.instance.s3_credentials },:default_url => '/images/:attachment/missing_:style.png'
+  has_attached_file :avatar,:default_url => "/assets/default_:style_avatar.png", :styles => { :medium => "300x300>", :thumb => "100x100>" }, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
