27926844b914adc975fa3f463c6aeb1e3f0c7481
daniel <zhangyihao1009@gmail.com>
2014-07-22 09:27:16 -0700

    action controller for asset

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :s3_credentials => Proc.new{|a| a.instance.s3_credentials },:default_url => "/assets/:style/defaultprofile.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :s3_credentials => Proc.new{|a| a.instance.s3_credentials },:default_url => ActionController::Base.helpers.asset_path('default_thumb_avatar.png')
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
