f0d3d38a5e09f94409f96ea502f81bd1a5983503
daniel <zhangyihao1009@gmail.com>
2014-07-22 13:36:32 -0700

    dotenv for environment setup

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,13 +1,13 @@
 class Asset < ActiveRecord::Base
 	belongs_to :product
-	has_attached_file :asset, :styles =>{:large => "640x480>", :medium =>"354x354#", :thumb =>"100x100>"}, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+	has_attached_file :asset, :styles =>{:large => "640x480>", :medium =>"354x354#", :thumb =>"100x100>"} #, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 	validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
 
 
 
-	def s3_credentials
-    {:bucket => "adjectivs-products", :access_key_id => "AKIAIWKGAOCVWSXLPDDA", :secret_access_key => "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"}
-    end
+	#def s3_credentials
+    #{:bucket => "adjectivs-products", :access_key_id => "AKIAIWKGAOCVWSXLPDDA", :secret_access_key => "28HcJ+ZDEZcLsa9WvtqiRZGxcR+BRGnN/Ja2tMFr"}
+    #end
 
      
        
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,9 +40,9 @@ SampleApp::Application.configure do
    config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['SECRET_KEY']
   }
 }
 end
